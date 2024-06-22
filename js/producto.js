28e140845704159f11f9a1a716f166b381cd36e4
guaingolotti <gusa@Air-de-guada.fibertel.com.ar>
2024-06-22 01:02:59 -0300

    vincule el saludo js en todos los html

--- js/producto.js
+++ js/producto.js
@@ -1,29 +1,35 @@
-document.addEventListener('DOMContentLoaded', function(){
-    let queryString = location.search;
-    let qsto = new URLSearchParams (queryString);
-    let id = qsto.get('id');
 
-    let url = "https://fakestoreapi.com/products/";
+let queryString = location.search;
+let qsto = new URLSearchParams (queryString);
+let id = qsto.get('id');
 
-fetch(url + id)
+let url = `https://fakestoreapi.com/products/${id}`;
+
+fetch(url)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
-        let nombreProducto = document.querySelector('h2');
-        let imagenProducto = document.querySelector('producto-imagen');
-        let descripcionProducto = document.querySelector('#producto-descripcion');
-        let precioProducto = document.querySelector('h3');
+        let dataString = JSON.stringify(data);
+        console.log(dataString);
 
-            // Actualizar los elementos del DOM con los datos del producto
-            nombreProducto.innerText = data.title;
-            imagenProducto.src = data.image;
-            descripcionProducto.innerText += data.description;
-            precioProducto.innerText += data.price;
+        let nombreProducto = document.querySelector('.producto-nombre');
+        let imagenProducto = document.querySelector('.producto-imagen');
+        let descripcionProducto = document.querySelector('.producto-descripcion');
+        let precioProducto = document.querySelector('.producto-precio');
+        let agregarCarrito = document.querySelector('.add-cart');
+
+        // Actualizar los elementos del DOM con los datos del producto
+        nombreProducto.innerText = "nombre:" + data.title;
+        imagenProducto.src = data.image;
+        descripcionProducto.innerText = "descripcion:" + data.description;
+        precioProducto.innerText = "precio:" + data.price;
+        agregarCarrito.href = "cart.html?id=" + data.id;
+        
     })
 
-.catch(function (error) {
+    .catch(function (error) {
     console.log("El error es: " + error);
-})
+    })
 
-});
+
