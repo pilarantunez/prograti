0fa5dbedd67e7e4cd972a65d5b650f033613b3e9
guaingolotti <gusa@MacBook-Air-de-guada.local>
2024-06-22 11:56:56 -0300

    saludo.js y modificacion del menu, vincule los id en todos los html

---
+++ js/saludo.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let email = localStorage.getItem('email');
+
+    if (email){
+        let bienvenidaSpan = document.getElementById('bienvenida');
+        let loginItem = document.getElementById('login');
+        let registroItem = document.getElementById('registro');
+
+        // construir el contenido HTML del mensaje de bienvenida y enlace de logout
+        bienvenidaSpan.innerHTML = `Bienvenido/a: ${email}<br><a href="#" id="logoutLink">Logout</a>`;
+
+        // agregar evento al enlace de logout
+        let logoutLink = document.getElementById('logoutLink');
+        logoutLink.addEventListener('click', function(e){
+            e.preventDefault();
+            localStorage.removeItem('email');
+            bienvenidaSpan.innerText = ''; 
+            loginItem.style.display = ''; 
+            registroItem.style.display = '';
+        });
+
+        // ocultar elementos de login y registro
+        loginItem.style.display = 'none';
+        registroItem.style.display = 'none';
+    }
+});
