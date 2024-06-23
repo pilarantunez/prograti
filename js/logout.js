505999b96c84bd443bbcc617cf7f298ef1c6ecca
matiasroyann <matiasroyan11@gmail.com>
2024-06-23 13:46:48 -0300

    comentarios en el logout

--- js/logout.js
+++ js/logout.js
@@ -1,7 +1,7 @@
-
+//captura el boton usando dom
 let logoutButton = document.querySelector('#logout-button');
 
-
+//cuando apreta el usuario el boton de logout, los datos guardados localmente se borraran y se lo redirige al login 
 logoutButton.addEventListener('click', function() {
     localStorage.clear(); 
     window.location.href = 'login.html';
